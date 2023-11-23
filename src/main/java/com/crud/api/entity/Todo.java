f9ef7b100d54e0f2e9bb82341e3c3b8b0dab4347
matheuspereira00 <matheusinhofcb12@gmail.com>
2023-11-23 09:48:13 -0300

    feat; add service

---
+++ src/main/java/com/crud/api/entity/Todo.java
@@ -0,0 +1,30 @@
+package com.crud.api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String descricao;
+
+    @Column
+    private Boolean realizado;
+
+    @Column
+    private int prioridade;
+}
---
+++ src/main/java/com/crud/api/service/TodoService.java
@@ -0,0 +1,32 @@
+package com.crud.api.service;
+
+import com.crud.api.entity.Todo;
+import com.crud.api.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+    public List<Todo> list(){
+        return todoRepository.findAll();
+    }
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
+
+}
